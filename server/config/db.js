1b3d231933dbd942782bac4de33b959b3504640d
Rojan <rojant061@gmail.com>
2025-01-10 21:04:55 +0545

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
-    );
+    console.log(`Connected To Mongodb Database ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error in Mongodb ${error}`.bgRed.white);
   }
